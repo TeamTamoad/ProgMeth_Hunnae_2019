ec9bcdb23fefcd7b0518ad666ab1a5147c34b778
Narawit Rakket <narawitrakket@hotmail.com>
2019-04-25 18:30:27 +0700

    Initiate sharedObject codes

---
+++ src/sharedObject/RenderableHolder.java
@@ -0,0 +1,55 @@
+package sharedObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
+
+
+public class RenderableHolder {
+	private static final RenderableHolder instance = new RenderableHolder();
+
+	private List<IRenderable> entities;
+	private Comparator<IRenderable> comparator;
+
+	static {
+		loadResource();
+	}
+
+	public RenderableHolder() {
+		entities = new ArrayList<IRenderable>();
+		comparator = (IRenderable o1, IRenderable o2) -> {
+			if (o1.getZ() > o2.getZ())
+				return 1;
+			return -1;
+		};
+	}
+
+	public static RenderableHolder getInstance() {
+		return instance;
+	}
+
+	public static void loadResource() {
+		
+	}
+
+	public void add(IRenderable entity) {
+		System.out.println("add");
+		entities.add(entity);
+		Collections.sort(entities, comparator);
+	}
+
+	public void update() {
+		for (int i = entities.size() - 1; i >= 0; i--) {
+			if (entities.get(i).isDestroyed())
+				entities.remove(i);
+		}
+	}
+
+	public List<IRenderable> getEntities() {
+		return entities;
+	}
+}
