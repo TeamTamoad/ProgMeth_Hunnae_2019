2f4f89e54edf862f2a01d04de72bd0b9aa4bfd0a
Narawit Rakket <narawitrakket@hotmail.com>
2019-04-25 18:46:14 +0700

    Add Image varibles

--- src/sharedObject/RenderableHolder.java
+++ src/sharedObject/RenderableHolder.java
@@ -14,6 +14,32 @@ public class RenderableHolder {
 
 	private List<IRenderable> entities;
 	private Comparator<IRenderable> comparator;
+	public static Image redTank1;
+	public static Image redTank2;
+	public static Image redTank3;
+	public static Image greenTank1;
+	public static Image greenank2;
+	public static Image greenTank3;
+	public static Image blueTank1;
+	public static Image blueTank2;
+	public static Image blueTank3;
+	public static Image sandTank1;
+	public static Image sandTank2;
+	public static Image sandTank3;
+	public static Image redShip1;
+	public static Image redShip2;
+	public static Image redShip3;
+	public static Image grrenShip1;
+	public static Image greenShip2;
+	public static Image greenShip3;
+	public static Image blueShip1;
+	public static Image blueShip2;
+	public static Image blueShip3;
+	public static Image whiteShip1;
+	public static Image whiteShip2;
+	public static Image whiteShip3;
+	
+	
 
 	static {
 		loadResource();
